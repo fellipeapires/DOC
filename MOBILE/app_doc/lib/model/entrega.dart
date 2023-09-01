47ee163a26813d9ab873a802c5520917e541ca3a
Fellipe <fellipeapires@gmail.com>
2023-09-01 17:21:34 -0300

    implementacao getCargaMobile

---
+++ MOBILE/app_doc/lib/model/entrega.dart
@@ -0,0 +1,31 @@
+class Entrega {
+  int? id;
+  String? codBarras;
+  String? codCliente;
+  String? sequencia;
+  String? roteiro;
+  String? endereco;
+  String? cep;
+  String? municipio;
+  int? grupoFaturamento;
+  int? idGrupoFaturamento;
+  int? idImportacao;
+  String? observacao;
+  int? pendente;
+
+  Entrega({
+    this.id,
+    this.codBarras,
+    this.codCliente,
+    this.sequencia,
+    this.roteiro,
+    this.endereco,
+    this.cep,
+    this.municipio,
+    this.grupoFaturamento,
+    this.idGrupoFaturamento,
+    this.idImportacao,
+    this.observacao,
+    this.pendente,
+  });
+}
--- MOBILE/app_doc/lib/screen/home_screen.dart
+++ MOBILE/app_doc/lib/screen/home_screen.dart
@@ -1,14 +1,80 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, unnecessary_string_interpolations, unnecessary_brace_in_string_interps, unnecessary_this, sort_child_properties_last
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, unnecessary_string_interpolations, unnecessary_brace_in_string_interps, unnecessary_this, sort_child_properties_last, unused_local_variable, use_build_context_synchronously, non_constant_identifier_names, avoid_print
 
+import 'dart:convert';
+
+import 'package:app_doc/model/entrega.dart';
+import 'package:app_doc/provider/entrega_provider.dart';
 import 'package:flutter/material.dart';
 import '../component/info_app.dart';
 import '../model/user.dart';
 import '../util/app_routes.dart';
 import '../util/utility.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final loading = ValueNotifier<bool>(false);
+  final entregaProvider = EntregaProvider();
+  //final List<Entrega> listaEntrega = [];
+
   void _setPage(BuildContext context, String appRoute, Object argumets) {
     Utility.setAppRouter(context, appRoute, argumets);
+  }
+
+  Future<void> getCargaEntrega(BuildContext context, int idUsuario) async {
+    await Utility.getStatusNet(context);
+    if (Utility.isNet) {
+      loading.value = true;
+      Entrega entrega;
+      try {
+        final future = entregaProvider.getCargaAPI(idUsuario);
+        future.then(
+          (response) => {
+            jsonDecode(response.body).forEach(
+              (element) => {
+                entrega = Entrega(),
+                entrega.id = int.tryParse(element['idEntrega'].toString())!,
+                entrega.idImportacao = int.tryParse(element['idImportacao'].toString())!,
+                entrega.idGrupoFaturamento = int.tryParse(element['idGrupoFaturamento'].toString())!,
+                entrega.grupoFaturamento = int.tryParse(element['grupoFaturamento'].toString())!,
+                entrega.roteiro = element['roteiro'],
+                entrega.sequencia = element['sequencia'],
+                entrega.municipio = element['municipio'],
+                entrega.endereco = element['endereco'],
+                entrega.cep = element['cep'],
+                entrega.codCliente = element['codCliente'],
+                entrega.codBarras = element['codBarras'],
+                entrega.observacao = element['observacao'],
+                entrega.pendente = 1,
+                entregaProvider.insert({
+                  'id': entrega.id,
+                  'grupoFaturamento': entrega.grupoFaturamento,
+                  'idImportacao': entrega.idImportacao,
+                  'roteiro': entrega.roteiro,
+                  'idGrupoFaturamento': entrega.idGrupoFaturamento,
+                  'sequencia': entrega.sequencia,
+                  'municipio': entrega.municipio,
+                  'endereco': entrega.endereco,
+                  'cep': entrega.cep,
+                  'codCliente': entrega.codCliente,
+                  'codBarras': entrega.codBarras,
+                  'observacao': entrega.observacao,
+                  'pendente': entrega.pendente
+                }),
+              },
+            ),
+            loading.value = false,
+          },
+        );
+      } catch (Exc) {
+        print('$Exc');
+      }
+    } else {
+      Utility.snackbar(context, 'SEM CONEXAO COM A INTERNET!');
+    }
   }
 
   @override
@@ -17,6 +83,16 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('HOME'),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.download_sharp,
+              size: 40,
+            ),
+            padding: const EdgeInsets.fromLTRB(1, 1, 25, 1),
+            onPressed: () => getCargaEntrega(context, user.id!),
+          ),
+        ],
       ),
       body: Stack(
         children: [
